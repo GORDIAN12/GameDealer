3ae6fe2860bf27fa43e98f52ca5e02e586125db1
David Gordian <davidgordian667@gmail.com>
2025-01-02 22:11:38 -0600

    resolve problem with origin

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('', views.index, name='index'),
+    path('origin', views.origin, name='origin'),
     path('producto/<int:producto_id>/', views.detail_product, name='detail_product'),
     ]

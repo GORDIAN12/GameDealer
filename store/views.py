5a53fd45cc1d95e50ccaa5f191b71a3751d9022f
David Gordian <davidgordian667@gmail.com>
2025-01-02 22:06:23 -0600

    problen with origin page

--- store/views.py
+++ store/views.py
@@ -7,6 +7,10 @@ def index(request):
     products=Product.objects.all()
     return render(request, 'index.html', {'products': products})
 
+def origin(request):
+    return render(request, 'origin.html')
+
+
 def detail_product(request, producto_id):
     producto = get_object_or_404(Product, id=producto_id)
     return render(request, 'detail_product.html', {'producto': producto})

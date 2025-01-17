900a3d482580a11f17e4915fafaec84c2e6c8633
David Gordian <davidgordian667@gmail.com>
2025-01-17 11:48:07 -0600

    update project

--- store/views.py
+++ store/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.http import HttpResponse
 from django.views import View
 from .models import Product
 import stripe
 from django.conf import settings
+from django.http.response import JsonResponse # new
+from django.views.decorators.csrf import csrf_exempt # new
+from django.views.generic.base import TemplateView
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def stripe_config(request):
+    if request.method == 'GET':
+        stripe_config = {'publicKey': settings.STRIPE_PUBLIC_KEY}
+        return JsonResponse(stripe_config, safe=False)
 
 def index(request):
     products=Product.objects.all()
@@ -13,19 +22,39 @@ def index(request):
 
 def origin(request):
     return render(request, 'origin.html')
+
+@csrf_exempt
+def create_checkout_session(request):
+    if request.method == 'GET':
+        domain_url = 'http://localhost:8000/home'
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'cancelled/',
+                payment_method_types=['card'],
+                mode='payment',
+                line_items=[
+                    {
+                        'price': 'price_1QhIBLHI987lTkKBCIO4pqkx',
+                        'quantity': 1,
+                    }
+                ]
+            )
+            return JsonResponse({'sessionId': checkout_session['id']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+
+
 def payment_succesful(request):
     return render(request, 'payment_succesful.html')
 def payment_cancelled(request):
     return render(request, 'payment_cancelled.html')
 
+def home(request):
+    return render(request, 'home.html')
 
 
 
-def detail_product(request, producto_id):
-    producto = get_object_or_404(Product, id=producto_id)
-    product_id='prod_RaT7xZiGNSmeww'
-    product=stripe.Product.retrieve(product_id)
-    prices=stripe.Price.list(product=product_id)
-    price=prices.data[0]
-    product_price=price.unit_amount/100.0
-    return render(request, 'detail_product.html', {'product': product,'product_price': product_price})
+
+

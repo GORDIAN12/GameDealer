1639b14a48706b8153d51d66faefe64cd35d013f
David Gordian <davidgordian667@gmail.com>
2025-01-29 12:25:09 -0600

    update navbar view product with authenticate

--- store/views.py
+++ store/views.py
@@ -35,7 +35,7 @@ def login_view(request):
         user=authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('dashboard')
+            return redirect('product')
         else:
             messages.error(request, 'usuario o contraseña incorrecto')
 
